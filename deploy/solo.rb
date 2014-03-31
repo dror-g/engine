2ba8b67ed77813ff828acb7b6b697639e4fb373c
dror-g <dror.g@zend.com>
2014-03-31 13:29:37 +0300

    Update solo.rb

--- deploy/solo.rb
+++ deploy/solo.rb
@@ -8,8 +8,8 @@ base_dir = File.expand_path(File.join(File.dirname(__FILE__), '..')) # project r
 
 file_cache_path   File.join(base_dir, 'tmp', 'cache') # it's possible that this dir doesn't exist? if so, either another dir will work or you could create it.
 cookbook_path     File.join(base_dir, 'cookbooks') # put your cookbooks dir in the root of the project, if possible. Another location can be used as well if this causes problems.
-node_name         DNApi.from(File.read("/etc/chef/dna.json")).id
-inode_name "i-d54e0f84"
+#node_name         DNApi.from(File.read("/etc/chef/dna.json")).id
+node_name "i-d54e0f84"
 {
   "name" => "my_node",
   "run_list" => ["recipe[zendserver::single]"]
