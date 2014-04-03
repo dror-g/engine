a43f28cee51e9f9dbfda87c5f12e7b8c8bc19b13
Dror Gensler <dror.g@zend.com>
2014-04-03 13:13:15 +0300

    cluster

--- cookbooks/zendserver/recipes/clusterjoin.rb
+++ cookbooks/zendserver/recipes/clusterjoin.rb
@@ -6,13 +6,14 @@ key_secret   = node[:zendserver][:apikeysecret]
 db_host 	 = nore[:zendserver][:dbhost]
 db_user 	 = nore[:zendserver][:dbusername]
 db_pass 	 = nore[:zendserver][:dbpassword]
+db_name 	 = nore[:zendserver][:dbname]
 
 # check before install that a suitable key is provided, if not bail out
 Chef::Application.fatal!("Zend Server db_host missing", 2) if db_host.nil? || db_host.empty?
 Chef::Application.fatal!("Zend Server db_user missing", 2) if db_user.nil? || db_user.empty?
 Chef::Application.fatal!("Zend Server db_pass missing", 2) if db_pass.nil? || db_pass.empty?
 
-join_command = "#{node[:zendserver][:zsmanage]} cluster-add-to-server -N #{key_name} -K #{key_secret} -n #{node['hostname']} -i #{node['ipaddress']} -o #{db_host} -u #{db_user} -p #{db_pass} -s"
+join_command = "#{node[:zendserver][:zsmanage]} cluster-add-to-server -N #{key_name} -K #{key_secret} -n #{node['hostname']} -i #{node['ipaddress']} -o #{db_host} -u #{db_user} -p #{db_pass} -d #{db_name} -s"
 
 execute "cluster-join-server" do
 	log "Adding server node to cluster"
